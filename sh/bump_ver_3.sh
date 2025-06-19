c406faba76d71f973c36a665d3eb90b65e40d30e
Vladimir Zhlobentsev <vladimir.zhlobentsev@gmail.com>
2025-06-19 03:04:28 +0300

    npm publish improvement

--- sh/bump_ver_3.sh
+++ sh/bump_ver_3.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# inject .env vars
+ENV="${NODE_ENV:-development}"
+source "$PWD/.env" 2> /dev/null
+source "$PWD/.env.$ENV" 2> /dev/null
+
+export NPM_TOKEN
+
 # run base njt script
 source "${PWD}"/node_modules/@dnclive/node-js-tools/sh/bump_ver_3.sh "$@"
